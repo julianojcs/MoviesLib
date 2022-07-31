4def6165d6cacf9018a56e93df7359cb859a970b
Juliano Costa Silva <apfjuliano@gmail.com>
2022-07-31 14:23:18 -0300

    adding images, imageIcons,TableView and TableViewController

---
+++ MoviesLib/MoviesTableViewController.swift
@@ -0,0 +1,97 @@
+//
+//  MoviesTableViewController.swift
+//  MoviesLib
+//
+//  Created by Juliano Costa Silva on 31/07/22.
+//
+
+import UIKit
+
+class MoviesTableViewController: UITableViewController {
+    var movies: [Movie] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadMovies()
+    }
+    
+    func loadMovies() {
+        guard let url = Bundle.main.url(forResource: "movies", withExtension: "json") else {
+            return
+        }
+        do {
+            let data = try Data(contentsOf: url)
+            movies = try JSONDecoder().decode([Movie].self, from: data)
+        } catch {
+            print(error)
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let movie = movies[indexPath.row]
+        cell.textLabel?.text = movie.title
+        cell.detailTextLabel?.text = movie.duration
+        
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MoviesLib/ViewController.swift
+++ MoviesLib/ViewController.swift
@@ -8,27 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var movies: [Movie] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadMovies()
     }
     
-    func loadMovies() {
-        guard let url = Bundle.main.url(forResource: "movies", withExtension: "json") else {
-            return
-        }
-        do {
-            let data = try Data(contentsOf: url)
-            movies = try JSONDecoder().decode([Movie].self, from: data)
-            for movie in movies {
-                print(movie.title)
-            }
-        } catch {
-            print(error)
-        }
-    }
 
 }
 
